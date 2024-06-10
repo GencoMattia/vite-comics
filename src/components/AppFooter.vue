21b7a6b35fda50eeb5be92129de631b589558c39
GencoMattia <gencomattia@hotmail.it>
2024-06-10 17:59:20 +0200

    Added logo-links lists

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,142 @@
 export default {
     data() {
         return {
+            DcComicsList: [
+                {
+                    id: 1,
+                    title: "Characters",
+                    active: false
+                },
+                {
+                    id: 2,
+                    title: "Comics",
+                    active: true
+                },
+                {
+                    id: 3,
+                    title: "Movies",
+                    active: false
+                },
+                {
+                    id: 4,
+                    title: "Tv",
+                    active: false
+                },
+                {
+                    id: 5,
+                    title: "Games",
+                    active: false
+                },
+                {
+                    id: 6,
+                    title: "Videos",
+                    active: false
+                },
+                {
+                    id: 7,
+                    title: "News",
+                    active: false
+                },
+            ],
 
+            ShopList: [
+                {
+                    id: 1,
+                    title: "Shop DC",
+                    active: false
+                },
+                {
+                    id: 2,
+                    title: "Shop DC Collectibles",
+                    active: false
+                },
+            ],
+
+            DcList: [
+                {
+                    id: 1,
+                    title: "Terms Of Use",
+                    active: false
+                },
+                {
+                    id: 2,
+                    title: "Privacy Policy (New)",
+                    active: true
+                },
+                {
+                    id: 3,
+                    title: "Ad Choices",
+                    active: false
+                },
+                {
+                    id: 4,
+                    title: "Advertising",
+                    active: false
+                },
+                {
+                    id: 5,
+                    title: "Jobs",
+                    active: false
+                },
+                {
+                    id: 6,
+                    title: "Subscriptions",
+                    active: false
+                },
+                {
+                    id: 7,
+                    title: "Talent Workshops",
+                    active: false
+                },
+                {
+                    id: 8,
+                    title: "CPSC Certificates",
+                    active: false
+                },
+                {
+                    id: 9,
+                    title: "Ratings",
+                    active: false
+                },
+                {
+                    id: 10,
+                    title: "Shop Help",
+                    active: false
+                },
+                {
+                    id: 11,
+                    title: "Contact Us",
+                    active: false
+                },
+            ],
+
+            SiteList: [
+                {
+                    id: 1,
+                    title: "DC",
+                    active: false
+                },
+                {
+                    id: 2,
+                    title: "MAD Magazines",
+                    active: false
+                },
+                {
+                    id: 3,
+                    title: "DC Kids",
+                    active: false
+                },
+                {
+                    id: 4,
+                    title: "DC Universe",
+                    active: false
+                },
+                {
+                    id: 5,
+                    title: "DC Power Visa",
+                    active: false
+                },
+            ],
         }
     }
 }
@@ -13,7 +148,46 @@ export default {
         <div class="container">
             <section class="logo-links">
                 <nav class="site-links">
-
+                    <div class="dc-comics-list">
+                        <h2>
+                            DC Comics
+                        </h2>
+                        <ul>
+                            <li v-for="link in DcComicsList" key="id">
+                                {{ link.title }}
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="dc-comics-list">
+                        <h2>
+                            Shop
+                        </h2>
+                        <ul>
+                            <li v-for="link in ShopList" key="id">
+                                {{ link.title }}
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="dc-comics-list">
+                        <h2>
+                            DC
+                        </h2>
+                        <ul>
+                            <li v-for="link in DcList" key="id">
+                                {{ link.title }}
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="dc-comics-list">
+                        <h2>
+                            Sites
+                        </h2>
+                        <ul>
+                            <li v-for="link in SiteList" key="id">
+                                {{ link.title }}
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
                 <div class="footer-logo">
                     <img src="../assets/img/dc-logo-bg.png" alt="footer-logo">
@@ -46,5 +220,9 @@ export default {
         .footer-logo {
             width: 50%;
         }
+    }
+
+    h2 {
+        text-transform: uppercase;
     }
 </style>
