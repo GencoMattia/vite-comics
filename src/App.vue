18bafdaaa9ec072d89821fcefc4de16682c8cee6
GencoMattia <gencomattia@hotmail.it>
2024-06-10 17:10:02 +0200

    added header navbar list

--- src/App.vue
+++ src/App.vue
@@ -27,5 +27,17 @@ export default {
 </template>
 
 <style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
+  body {
+    font-family: sans-serif;
+  }
+
+  ul {
+    list-style: none;
+  }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,58 @@
 export default {
     data() {
         return {
-    
+            navbarLinks: [
+                {
+                    id: 1,
+                    title: "Characters",
+                    active: false
+                },
+                {
+                    id: 2,
+                    title: "Comics",
+                    active: true
+                },
+                {
+                    id: 3,
+                    title: "Movies",
+                    active: false
+                },
+                {
+                    id: 4,
+                    title: "Tv",
+                    active: false
+                },
+                {
+                    id: 5,
+                    title: "Games",
+                    active: false
+                },
+                {
+                    id: 6,
+                    title: "Collectibles",
+                    active: false
+                },
+                {
+                    id: 7,
+                    title: "Videos",
+                    active: false
+                },
+                {
+                    id: 8,
+                    title: "Fans",
+                    active: false
+                },
+                {
+                    id: 9,
+                    title: "News",
+                    active: false
+                },
+                {
+                    id: 10,
+                    title: "Shop",
+                    active: false
+                },
+            ]
         };
     }
 };
@@ -10,9 +61,16 @@ export default {
 
 <template>
     <header>
-        <h1>
-            Io sono un Header
-        </h1>
+        <section class="logo">
+            <img src="../assets/img/dc-logo.png" alt="header-logo">
+        </section>
+        <nav class="navbar">
+            <ul>
+                <li v-for="link in navbarLinks" key="id">
+                    {{ link.title.toUpperCase() }}
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
